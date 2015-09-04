591ccbd6d14b91a9300f739edd349f9596c95863
gennadiy.dubina <Gennadiy.Dubina@dataart.com>
2015-09-04 23:42:00 +0300

    update time

--- deploy-kube.sh
+++ deploy-kube.sh
@@ -1,7 +1,7 @@
 export JUJU_REPOSITORY=$PWD/charms/
 
 juju set-env "default-series=trusty"
-juju set-constraints mem=1G
+juju set-constraints mem=768M
 
 juju-deployer -c bundle-demo-kube.yaml -c config-demo.yaml demo
 
