eb63485acd290451f56d97ecd03ef6ef3b6a6840
Gennadiy Dubina <gdubina@dataart.com>
2015-10-26 21:03:16 +0200

    push to charm store; update remote script

--- remote/deploy-tads2015-demo.sh
+++ remote/deploy-tads2015-demo.sh
@@ -1,15 +1,19 @@
 
-juju set-env "default-series=trusty"
-
-juju-deployer -c tads2015-demo/bundle.yaml -c config-demo.yaml tads2015-demo
-
-JUJU_CUR_ENV=`cat ~/.juju/current-environment`
+if [ "$#" -ge 1 ]; then
+    JUJU_CUR_ENV=$1
+else
+    JUJU_CUR_ENV=`cat ~/.juju/current-environment`
+fi
 
 echo 'juju current env: '$JUJU_CUR_ENV
+
+juju set-env -e $JUJU_CUR_ENV "default-series=trusty"
+
+juju-deployer -e $JUJU_CUR_ENV -c tads2015-demo/bundle.yaml -c config-demo.yaml tads2015-demo
 
 JUJU_PASS=$(grep password ~/.juju/environments/$JUJU_CUR_ENV.jenv | sed 's/.*: //g')
 
 echo 'juju admin password: '$JUJU_PASS
-juju set monitor-server JUJU_API_PASSWORD=$JUJU_PASS
+juju set -e $JUJU_CUR_ENV monitor-server JUJU_API_PASSWORD=$JUJU_PASS
 
 ./bind-elaststic-ip.sh
