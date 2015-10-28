ea699c1ab5c63dd25c1bf8f7e1a539508b8bcaaa
Gennadiy Dubina <gdubina@dataart.com>
2015-10-28 20:37:07 +0000

    update bundle

--- remote/deploy-tads2015-demo.sh
+++ remote/deploy-tads2015-demo.sh
@@ -9,12 +9,14 @@ echo 'juju current env: '$JUJU_CUR_ENV
 
 juju set-env -e $JUJU_CUR_ENV "default-series=trusty"
 
-juju-deployer -e $JUJU_CUR_ENV -c tads2015-demo/bundle.yaml -c config-demo.yaml
+juju-deployer -e $JUJU_CUR_ENV -c tads2015-demo/bundle.yaml
 
 JUJU_PASS=$(grep password ~/.juju/environments/$JUJU_CUR_ENV.jenv | sed 's/.*: //g')
 
 echo 'juju admin password: '$JUJU_PASS
 juju set -e $JUJU_CUR_ENV monitor-server JUJU_API_PASSWORD=$JUJU_PASS
+juju set -e $JUJU_CUR_ENV telscale-restcomm static_ip=52.28.141.175
+
 juju add-relation -e $JUJU_CUR_ENV monitor-server:api-server juju-gui:web
 
 ./bind-elaststic-ip.sh
