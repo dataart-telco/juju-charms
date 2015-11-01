76ea3eaf89f53dfd46ea1c775933917d4b9eab73
Gennadiy Dubina <gdubina@dataart.com>
2015-11-01 10:01:32 +0000

    udapte deploy script

--- remote/deploy-tads2015-demo.sh
+++ remote/deploy-tads2015-demo.sh
@@ -1,21 +1,21 @@
 USE_EIP=0
 
 print_usage(){
-    printf "usage: [-eip] <juju env>\n"
+    printf "usage: [-i] [-e juju env]\n"
 }
 
-while getopts 'eip:' flag; do
+while getopts 'ie:' flag; do
     case "${flag}" in
-        eip) USE_EIP=1 ;;
+        i)  USE_EIP=1 ;;
+        e)  JUJU_CUR_ENV=$OPTARG ;;
         *)  print_usage
             exit 1
             ;;
    esac
 done
 
-if [ "$#" -ge 1 ]; then
-    JUJU_CUR_ENV=$1
-else
+if [ -z "$JUJU_CUR_ENV" ]; then
+    echo "Get default Jujuj environment"
     JUJU_CUR_ENV=`cat ~/.juju/current-environment`
 fi
 
@@ -31,7 +31,10 @@ echo 'juju admin password: '$JUJU_PASS
 juju set -e $JUJU_CUR_ENV monitor-server JUJU_API_PASSWORD=$JUJU_PASS
 juju add-relation -e $JUJU_CUR_ENV monitor-server:api-server juju-gui:web
 
-if [ $USE_EAPI -eq 1 ]; then
+if [ $USE_EIP -eq 1 ]; then
+
+    echo 'Try to bind elastic ip to restcomm'
+
     juju set -e $JUJU_CUR_ENV telscale-restcomm static_ip=52.28.141.175
     ./bind-elaststic-ip.sh
 fi
