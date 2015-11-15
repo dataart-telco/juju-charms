1b1f5a8418e77536956718e97e4f581e864afde6
Gennadiy Dubina <gdubina@dataart.com>
2015-11-15 12:11:28 +0000

    fix number in script

--- remote/deploy-recorder.sh
+++ remote/deploy-recorder.sh
@@ -3,10 +3,10 @@ JUJU_ENV=$1
 juju set-env -e $JUJU_ENV "default-series=trusty"
 
 echo 'conference-recorder:
-  user: "bob"
+  user: "recorder"
   password: "1234"
   proxy: "52.28.141.175:5080"
-  number: "+5555"
+  number: "5555"
 ' > /tmp/conference-recorder.yaml
 
 juju deploy -e $JUJU_ENV cs:~tads2015dataart/trusty/conference-recorder-2 conference-recorder --config /tmp/conference-recorder.yaml
