25ddcf87ee621729718377163500a3f3ed452257
Gennadiy Dubina <hamsterksu@gmail.com>
2016-01-27 23:59:53 +0200

    use ansible

--- deploy-mesos-local.sh
+++ deploy-mesos-local.sh
@@ -1,19 +1,20 @@
 export JUJU_REPOSITORY=$PWD/charms/
 
 if [ "$#" -ge 1 ]; then
-	JUJU_CUR_ENV=$1
+    JUJU_CUR_ENV=$1
 else
-	JUJU_CUR_ENV=`cat ~/.juju/current-environment`
+    JUJU_CUR_ENV=`cat ~/.juju/current-environment`
 fi
 
 echo 'juju current env: '$JUJU_CUR_ENV
 
 juju set-env -e $JUJU_CUR_ENV "default-series=trusty"
-#juju set-constraints "mem=512M"
+juju set-constraints "mem=512M"
 #juju set-constraints 'instance-type=m1.small'
 
 
-juju-deployer -e $JUJU_CUR_ENV -c bundle-demo-mesos-local.yaml -c config-demo.yaml demo
+juju-deployer -e $JUJU_CUR_ENV -c bundle-demo-mesos-local.yaml
+# -c config-demo.yaml demo
 
 JUJU_PASS=$(grep password ~/.juju/environments/$JUJU_CUR_ENV.jenv | sed 's/.*: //g')
 
--- deploy-test.sh
+++ deploy-test.sh
@@ -4,5 +4,5 @@ juju set-env "default-series=trusty"
 juju set-constraints "mem=512M"
 #juju set-constraints 'instance-type=m1.small'
 
-juju deploy local:trusty/monitor-server2 monitor-server
+juju deploy local:trusty/monitor-server monitor-server
 juju add-relation monitor-server:api-server juju-gui:web
