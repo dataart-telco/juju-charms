9b112694fe1acf64cb335d64c0229d0f6baeb279
Gennadiy Dubina <hamsterksu@gmail.com>
2016-02-09 07:45:29 +0200

    Add restcomm-4mesos

---
+++ deploy-mesos-cluster-only.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+export JUJU_REPOSITORY=$PWD/charms/
+
+if [ "$#" -ge 1 ]; then
+    JUJU_CUR_ENV=$1
+else
+    JUJU_CUR_ENV=`cat ~/.juju/current-environment`
+fi
+
+echo 'juju current env: '$JUJU_CUR_ENV
+
+juju set-env -e $JUJU_CUR_ENV "default-series=trusty"
+juju set-constraints "mem=512M"
+
+juju deploy --constraints="mem=3G" local:trusty/mesos-master
+juju deploy --constraints="mem=3G" local:trusty/mesos-slave
+
+
+juju add-relation mesos-master mesos-slave
